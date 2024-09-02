3f4fe234958b6950d2a430074bec7f7c3290a0e0
unknown <mamartin11@eafit.edu.co>
2024-09-01 20:45:49 -0500

    roll a ball tutorial

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using TMPro;
-
+//Commit 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
